ffd4bee698a7dd5ef3756eebe3d31ed0fec8331e
Georgii Tishenin <georgii.tishenin@eonerc.rwth-aachen.de>
2024-05-19 18:01:48 +0200

    Support eigenvalue extraction by EMT Ph1 R,L,C components and voltage source
    Signed-off-by: Georgii Tishenin <georgii.tishenin@eonerc.rwth-aachen.de>

--- dpsim-models/src/EMT/EMT_Ph1_VoltageSource.cpp
+++ dpsim-models/src/EMT/EMT_Ph1_VoltageSource.cpp
@@ -72,6 +72,18 @@ void EMT::Ph1::VoltageSource::mnaCompApplySystemMatrixStamp(
   }
 }
 
+void EMT::Ph1::VoltageSource::stampBranchNodeIncidenceMatrix(
+    Matrix &branchNodeIncidenceMatrix) {
+  if (terminalNotGrounded(0)) {
+    branchNodeIncidenceMatrix(mBranchIdx, matrixNodeIndex(0)) = 1.0;
+  }
+  if (terminalNotGrounded(1)) {
+    branchNodeIncidenceMatrix(mBranchIdx, matrixNodeIndex(1)) = -1.0;
+  }
+}
+
+void EMT::Ph1::VoltageSource::setBranchIdx(UInt i) { mBranchIdx = i; }
+
 void EMT::Ph1::VoltageSource::mnaCompApplyRightSideVectorStamp(
     Matrix &rightVector) {
   Math::setVectorElement(rightVector, mVirtualNodes[0]->matrixNodeIndex(),
