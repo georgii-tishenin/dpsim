f0a1c6bb4d65126ea4efb1f36bf2ef6fc6c79ab5
Georgii Tishenin <georgii.tishenin@eonerc.rwth-aachen.de>
2025-07-23 16:49:31 +0200

    IdealTransformerVariable ratio: remove commented out code and intialization
    Signed-off-by: Georgii Tishenin <georgii.tishenin@eonerc.rwth-aachen.de>

--- dpsim-models/src/EMT/EMT_Ph1_IdealTransformer_VariableRatio.cpp
+++ dpsim-models/src/EMT/EMT_Ph1_IdealTransformer_VariableRatio.cpp
@@ -28,51 +28,15 @@ void EMT::Ph1::IdealTransformerVariableRatio::setParameters(Real N) {
 
 void EMT::Ph1::IdealTransformerVariableRatio::initializeFromNodesAndTerminals(
     Real frequency) {
-
-  // Component parameters are referred to higher voltage side.
-  // Switch terminals to have terminal 0 at higher voltage side
-  // if transformer is connected the other way around.
-  if (Math::abs(*mN) < 1.) {
-    if (*mN == 0) {
-      *mN = 0.00001;
-    } else {
-      *mN = 1. / *mN;
-      std::shared_ptr<SimTerminal<Real>> tmp = mTerminals[0];
-      mTerminals[0] = mTerminals[1];
-      mTerminals[1] = tmp;
-      SPDLOG_LOGGER_INFO(mSLog, "Switching terminals to have first terminal at "
-                                "higher voltage side. Updated parameters: ");
-      SPDLOG_LOGGER_INFO(mSLog, "Turns Ratio = {} [ ]", std::abs(*mN));
-    }
-  }
-
-  // Set initial voltage of virtual node in between
-  mVirtualNodes[0]->setInitialVoltage(initialSingleVoltage(1) * *mN);
-
-  // Log initialization results
-  SPDLOG_LOGGER_INFO(
-      mSLog,
-      "--- Initialization ---\n"
-      "Terminal 0 voltage: {:s}\n"
-      "Terminal 1 voltage: {:s}\n"
-      "Virtual Node voltage: {:s}\n"
-      "--- Initialization finished ---",
-      Logger::phasorToString(initialSingleVoltage(0)),
-      Logger::phasorToString(initialSingleVoltage(1)),
-      Logger::phasorToString(mVirtualNodes[0]->initialSingleVoltage()));
 }
 
 void EMT::Ph1::IdealTransformerVariableRatio::mnaCompInitialize(
     Real omega, Real timeStep, Attribute<Matrix>::Ptr leftVector) {
   updateMatrixNodeIndices();
-  //**mRightVector = Matrix::Zero(0, 0); //If you uncomment this line, mnaCompPreStep will not be called every time step.
 }
 
 void EMT::Ph1::IdealTransformerVariableRatio::mnaCompApplySystemMatrixStamp(
     SparseMatrixRow &systemMatrix) {
-
-  //    *mN = *mN + ( mTimeStep / 2 ) * (mOldVoltage + mVoltage);
-  //    mRatio->set(*mN);
 
   // Ideal transformer equations
   if (terminalNotGrounded(0)) {
@@ -149,13 +113,10 @@ void EMT::Ph1::IdealTransformerVariableRatio::mnaCompPostStep(
   mnaCompUpdateVoltage(**leftVector);
   mnaCompUpdateCurrent(**leftVector);
 
-  //updateVoltages(**leftVector);
-
   if (mInductor != nullptr) {
 
     mOldVoltage = mVoltage;
     // Update the actual voltage of the terminal
-    //auto idx = mInductor->terminal(0)->matrixNodeIndex();
     auto idx = mInductor->matrixNodeIndex(0);
     mVoltage = Math::realFromVectorElement(**leftVector, idx);
   }
@@ -163,17 +124,6 @@ void EMT::Ph1::IdealTransformerVariableRatio::mnaCompPostStep(
 
 void EMT::Ph1::IdealTransformerVariableRatio::mnaCompUpdateVoltage(
     const Matrix &leftVector) {
-  // v1 - v0
-  /*
-  (**mIntfVoltage)(0, 0) = 0;
-  (**mIntfVoltage)(0, 0) =
-      Math::realFromVectorElement(leftVector, matrixNodeIndex(1));
-  (**mIntfVoltage)(0, 0) = (**mIntfVoltage)(0, 0) -
-                           Math::realFromVectorElement(
-                               leftVector, mVirtualNodes[0]->matrixNodeIndex());
-  SPDLOG_LOGGER_DEBUG(mSLog, "Voltage {:s}",
-                      Logger::phasorToString((**mIntfVoltage)(0, 0)));
-                      */
 }
 
 void EMT::Ph1::IdealTransformerVariableRatio::mnaCompUpdateCurrent(
@@ -181,43 +131,11 @@ void EMT::Ph1::IdealTransformerVariableRatio::mnaCompUpdateCurrent(
 
 void EMT::Ph1::IdealTransformerVariableRatio::stampBranchNodeIncidenceMatrix(
     UInt branchIdx, Matrix &branchNodeIncidenceMatrix) {
-
-  /*
-  UInt primaryBranchIdx = branchIdx - 1;
-  UInt secondaryBranchIdx = branchIdx;
-
-  if (terminalNotGrounded(0)) {
-    branchNodeIncidenceMatrix(primaryBranchIdx, matrixNodeIndex(0)) = 1.0;
-  }
-    
-    branchNodeIncidenceMatrix(primaryBranchIdx, mVirtualNodes[0]->matrixNodeIndex()) = -1.0;
-  
-  if (terminalNotGrounded(1)) {
-  branchNodeIncidenceMatrix(secondaryBranchIdx, matrixNodeIndex(1)) = -1;
- }
-
-  branchNodeIncidenceMatrix(secondaryBranchIdx, mVirtualNodes[0]->matrixNodeIndex()) = 1;
-  */
 }
 
 void EMT::Ph1::IdealTransformerVariableRatio::setTimeStep(Real timeStep) {
   mTimeStep = timeStep;
 }
-
-/*
- // A function which serves to update the voltages which will be used in the mnaCompPreStep to calculate the new Ratio as a discrete integration
- // of the voltage source declared as a pointer in the header file
- void EMT::Ph1::IdealTransformerVariableRatio::updateVoltages(const Matrix &leftVector) {
-   if (mInductor != nullptr) {
-
-    mOldVoltage = mVoltage;
-    // Update the actual voltage of the terminal
-    auto idx = mInductor->terminal(0)->matrixNodeIndex();
-    mVoltage = Math::realFromVectorElement(**leftVector, idx);
-    //mVoltage = mVoltageSource->mVoltageRef->get().real();
-   }
- }
-*/
 
 void EMT::Ph1::IdealTransformerVariableRatio::mnaCompPreStep(
     Real time, Int timeStepCount) {
