59947c7c0320e5dbba6a56278dcf4a75a7af9285
Georgii Tishenin <georgii.tishenin@eonerc.rwth-aachen.de>
2025-07-23 16:03:08 +0200

    Add files for Composite_SynchGen example
    Signed-off-by: Georgii Tishenin <georgii.tishenin@eonerc.rwth-aachen.de>

--- dpsim-models/include/dpsim-models/Components.h
+++ dpsim-models/include/dpsim-models/Components.h
@@ -91,6 +91,9 @@
 #include <dpsim-models/EMT/EMT_Ph1_VoltageSource.h>
 #include <dpsim-models/EMT/EMT_Ph1_VoltageSourceNorton.h>
 #include <dpsim-models/EMT/EMT_Ph1_VoltageSourceRamp.h>
+#include <dpsim-models/EMT/EMT_Ph1_ParkTransformer.h>
+#include <dpsim-models/EMT/EMT_Ph1_IdealTransformer_VariableRatio.h>
+#include <dpsim-models/EMT/EMT_Ph1_InertiaMoment.h>
 
 #include <dpsim-models/EMT/EMT_Ph3_AvVoltSourceInverterStateSpace.h>
 #include <dpsim-models/EMT/EMT_Ph3_AvVoltageSourceInverterDQ.h>
---
+++ dpsim-models/include/dpsim-models/EMT/EMT_Ph1_IdealTransformer_VariableRatio.h
@@ -0,0 +1,105 @@
+
+#pragma once
+
+#include <iostream>
+
+#include <dpsim-models/Attribute.h>
+#include <dpsim-models/AttributeList.h>
+#include <dpsim-models/Definitions.h>
+#include <dpsim-models/MNASimPowerComp.h>
+#include <dpsim-models/Solver/EigenvalueCompInterface.h>
+#include <dpsim-models/Solver/MNAInterface.h>
+#include <dpsim-models/Solver/MNAVariableCompInterface.h>
+
+namespace CPS {
+namespace EMT {
+namespace Ph1 {
+
+class Inductor;
+
+class IdealTransformerVariableRatio
+    : public MNASimPowerComp<Real>,
+      public MNAVariableCompInterface,
+      public SharedFactory<IdealTransformerVariableRatio>,
+      public EigenvalueCompInterface {
+public:
+  /// Turns ratio (v1 = v2 * ratio)
+  const std::shared_ptr<Real> mN;
+
+  const typename Attribute<Real>::Ptr mRatio;
+
+  // In order to get the discrete integration of a voltage (flux) as the turns ratio of the ideal transformer
+
+  Real mTimeStep = 0.0;
+
+  Real mOldVoltage = 0.0;
+
+  Real mVoltage = 0.0;
+
+  bool mNegativeSpeedTermVoltageFlag = false;
+
+  std::shared_ptr<CPS::EMT::Ph1::Inductor> mInductor;
+
+  void setStatorInductor(const std::shared_ptr<CPS::EMT::Ph1::Inductor> &pt) {
+    mInductor = pt;
+  }
+
+  void setVoltageNegative(bool flag) { mNegativeSpeedTermVoltageFlag = flag; }
+  void setTimeStep(Real timeStep);
+  //void updateVoltages(const Matrix &leftVector);
+
+  /// Defines UID, name and logging level
+  IdealTransformerVariableRatio(String uid, String name,
+                                Logger::Level logLevel = Logger::Level::off);
+
+  /// Defines name and logging level
+  IdealTransformerVariableRatio(String name,
+                                Logger::Level logLevel = Logger::Level::off)
+      : IdealTransformerVariableRatio(name, name, logLevel) {}
+
+  // #### General ####
+  /// Defines component parameters
+  void setParameters(Real N);
+
+  /// Initializes component from power flow data
+  void initializeFromNodesAndTerminals(Real frequency) override;
+
+  // #### MNA section ####
+  /// Initializes internal variables of the component
+  void mnaCompInitialize(Real omega, Real timeStep,
+                         Attribute<Matrix>::Ptr leftSideVector) override;
+  /// Stamps system matrix
+  void mnaCompApplySystemMatrixStamp(SparseMatrixRow &systemMatrix) override;
+  /// Stamps right side (source) vector
+  void mnaCompApplyRightSideVectorStamp(Matrix &rightVector) override {}
+  /// Update interface voltage from MNA system result
+  void mnaCompUpdateVoltage(const Matrix &leftVector) override;
+  /// Update interface current from MNA system result
+  void mnaCompUpdateCurrent(const Matrix &leftVector) override;
+  void mnaCompPostStep(Real time, Int timeStepCount,
+                       Attribute<Matrix>::Ptr &leftVector) override;
+  /// Add MNA post step dependencies
+  void
+  mnaCompAddPostStepDependencies(AttributeBase::List &prevStepDependencies,
+                                 AttributeBase::List &attributeDependencies,
+                                 AttributeBase::List &modifiedAttributes,
+                                 Attribute<Matrix>::Ptr &leftVector) override;
+
+  /// Add MNA pre step dependencies
+  void mnaCompAddPreStepDependencies(
+      AttributeBase::List &prevStepDependencies,
+      AttributeBase::List &attributeDependencies,
+      AttributeBase::List &modifiedAttributes) override;
+
+  void mnaCompPreStep(Real time, Int timeStepCount) override;
+
+  // #### Implementation of eigenvalue component interface ####
+  void stampBranchNodeIncidenceMatrix(UInt branchIdx,
+                                      Matrix &branchNodeIncidenceMatrix) final;
+
+  /// Mark that parameter changes so that system matrix is updated
+  Bool hasParameterChanged() override { return true; }
+};
+} // namespace Ph1
+} // namespace EMT
+} // namespace CPS
