b3ae35493457a07e1c83d879ebe2e31966d68bb0
Georgii Tishenin <georgii.tishenin@eonerc.rwth-aachen.de>
2025-08-29 11:58:49 +0200

    Add current controlled torque source (WIP)
    Signed-off-by: Georgii Tishenin <georgii.tishenin@eonerc.rwth-aachen.de>

---
+++ dpsim-models/include/dpsim-models/EMT/EMT_Ph1_CurrentControlledTorqueSource.h
@@ -0,0 +1,77 @@
+
+#pragma once
+
+#include <iostream>
+
+#include <dpsim-models/Attribute.h>
+#include <dpsim-models/AttributeList.h>
+#include <dpsim-models/Definitions.h>
+#include <dpsim-models/MNASimPowerComp.h>
+#include <dpsim-models/Solver/EigenvalueCompInterface.h>
+#include <dpsim-models/Solver/MNAInterface.h>
+#include <dpsim-models/Solver/MNAVariableCompInterface.h>
+
+namespace CPS {
+namespace EMT {
+namespace Ph1 {
+
+class Inductor;
+
+class CurrentControlledTorqueSource : public MNASimPowerComp<Real>,
+                                      public MNAVariableCompInterface,
+                                      public SharedFactory<VoltageSpeedTerm>,
+                                      public EigenvalueCompInterface {
+public:
+  const typename Attribute<Real>::Ptr mCoefficientCurrent;
+
+  std::shared_ptr<Inductor> mInductor;
+
+  Real mCoefficient;
+
+  bool mIsNegative = false;
+
+  /// Defines UID, name and logging level
+  CurrentControlledTorqueSource(String uid, String name,
+                                Logger::Level logLevel = Logger::Level::off);
+
+  /// Defines name and logging level
+  CurrentControlledTorqueSource(String name,
+                                Logger::Level logLevel = Logger::Level::off)
+      : CurrentControlledTorqueSource(name, name, logLevel) {}
+
+  // #### General ####
+  /// Sets coefficient
+  void setCoefficient(Real coefficient) { mCoefficient = coefficient; };
+
+  void setIsNegative(bool isNegative) { mIsNegative = isNegative; }
+
+  void setInductorForCurrentRerefence(const std::shared_ptr<Inductor> &pt) {
+    mInductor = pt;
+  }
+
+  // #### MNA section ####
+  /// Initializes internal variables of the component
+  void mnaCompInitialize(Real omega, Real timeStep,
+                         Attribute<Matrix>::Ptr leftSideVector) override;
+  /// Stamps system matrix
+  void mnaCompApplySystemMatrixStamp(SparseMatrixRow &systemMatrix) override;
+
+  void mnaCompPostStep(Real time, Int timeStepCount,
+                       Attribute<Matrix>::Ptr &leftVector) override;
+  /// Add MNA post step dependencies
+  void
+  mnaCompAddPostStepDependencies(AttributeBase::List &prevStepDependencies,
+                                 AttributeBase::List &attributeDependencies,
+                                 AttributeBase::List &modifiedAttributes,
+                                 Attribute<Matrix>::Ptr &leftVector) override;
+
+  // #### Implementation of eigenvalue component interface ####
+  void stampBranchNodeIncidenceMatrix(UInt branchIdx,
+                                      Matrix &branchNodeIncidenceMatrix) final;
+
+  // Mark that parameter changes so that system matrix is updated
+  Bool hasParameterChanged() override { return true; }
+};
+} // namespace Ph1
+} // namespace EMT
+} // namespace CPS
