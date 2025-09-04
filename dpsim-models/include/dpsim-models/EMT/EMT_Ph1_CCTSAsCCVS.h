949875055883724117e52635ee0af900ea291a02
Georgii Tishenin <georgii.tishenin@eonerc.rwth-aachen.de>
2025-09-04 17:04:54 +0200

    Add CCTS as CCVS
    Signed-off-by: Georgii Tishenin <georgii.tishenin@eonerc.rwth-aachen.de>

---
+++ dpsim-models/include/dpsim-models/EMT/EMT_Ph1_CCTSAsCCVS.h
@@ -0,0 +1,88 @@
+
+#pragma once
+
+#include <iostream>
+
+#include <dpsim-models/Attribute.h>
+#include <dpsim-models/AttributeList.h>
+#include <dpsim-models/Definitions.h>
+#include <dpsim-models/MNASimPowerComp.h>
+#include <dpsim-models/Solver/EigenvalueCompInterface.h>
+#include <dpsim-models/Solver/MNAInterface.h>
+#include <dpsim-models/Solver/MNAVariableCompInterface.h>
+
+namespace CPS {
+template <typename VarType> class SimNode;
+}
+
+namespace CPS {
+namespace EMT {
+namespace Ph1 {
+
+class CCTSAsCCVS
+    : public MNASimPowerComp<Real>,
+      public MNAVariableCompInterface,
+      public SharedFactory<CCTSAsCCVS>,
+      public EigenvalueCompInterface {
+public:
+
+  /// flux is turns ratio (v = flux * omega, torque = flux * i)
+  const typename Attribute<Real>::Ptr mFlux;
+  
+  std::shared_ptr<CPS::SimNode<Real>> mVoltageReferenceNode;
+
+  Real mCoefficient;
+
+  // In order to get the discrete integration of a voltage (flux)
+  Real mTimeStep = 0.0;
+
+  Real mOldVoltage = 0.0;
+
+  Real mVoltage = 0.0;
+
+  /// Defines UID, name and logging level
+  CCTSAsCCVS(String uid, String name,
+                                Logger::Level logLevel = Logger::Level::off);
+
+  /// Defines name and logging level
+  CCTSAsCCVS(String name,
+                                Logger::Level logLevel = Logger::Level::off)
+      : CCTSAsCCVS(name, name, logLevel) {}
+
+  // #### General ####
+  /// Sets coefficient
+  void setCoefficient(Real coefficient) { mCoefficient = coefficient; };
+
+  void setInitialFlux(Real flux);
+
+  void setVoltageReferenceNode(const std::shared_ptr<CPS::SimNode<Real>> &pt) {
+    mVoltageReferenceNode = pt;
+  }
+
+  // #### MNA section ####
+  /// Initializes internal variables of the component
+  void mnaCompInitialize(Real omega, Real timeStep,
+                         Attribute<Matrix>::Ptr leftSideVector) override;
+  /// Stamps system matrix
+  void mnaCompApplySystemMatrixStamp(SparseMatrixRow &systemMatrix) override;
+
+  void mnaCompPostStep(Real time, Int timeStepCount,
+                       Attribute<Matrix>::Ptr &leftVector) override;
+  /// Add MNA post step dependencies
+  void
+  mnaCompAddPostStepDependencies(AttributeBase::List &prevStepDependencies,
+                                 AttributeBase::List &attributeDependencies,
+                                 AttributeBase::List &modifiedAttributes,
+                                 Attribute<Matrix>::Ptr &leftVector) override;
+
+  // #### Implementation of eigenvalue component interface ####
+  void stampBranchNodeIncidenceMatrix(UInt branchIdx,
+                                      Matrix &branchNodeIncidenceMatrix) final;
+
+  // Mark that parameter changes so that system matrix is updated
+  Bool hasParameterChanged() override { return true; }
+
+};
+} // namespace Ph1
+} // namespace EMT
+} // namespace CPS
