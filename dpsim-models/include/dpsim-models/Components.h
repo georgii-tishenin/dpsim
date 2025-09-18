30e624b58c6c4330ba798c869854bd2edc7c2c6a
Georgii Tishenin <georgii.tishenin@eonerc.rwth-aachen.de>
2025-09-18 14:59:57 +0200

    Add time-lagging torque source
    Signed-off-by: Georgii Tishenin <georgii.tishenin@eonerc.rwth-aachen.de>

--- dpsim-models/include/dpsim-models/Components.h
+++ dpsim-models/include/dpsim-models/Components.h
@@ -95,6 +95,7 @@
 #include <dpsim-models/EMT/EMT_Ph1_CurrentControlledTorqueSource.h>
 #include <dpsim-models/EMT/EMT_Ph1_CCTSAsCCVS.h>
 #include <dpsim-models/EMT/EMT_Ph1_TimeLaggingVoltageSource.h>
+#include <dpsim-models/EMT/EMT_Ph1_TimeLaggingTorqueSource.h>
 
 #include <dpsim-models/EMT/EMT_Ph3_AvVoltSourceInverterStateSpace.h>
 #include <dpsim-models/EMT/EMT_Ph3_AvVoltageSourceInverterDQ.h>
